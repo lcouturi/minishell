a097e8cd75def270c4b88729a72954f93c4cc799
Léopold Couturier-Otis <leo.couturier.otis@gmail.com>
2024-01-16 16:18:45 -0500

    added PATH support!

--- main.c
+++ main.c
@@ -33,16 +33,74 @@ static void	cmd_exit(char **args)
 	exit(EXIT_SUCCESS);
 }
 
+static char	*locate_path(char **args, char **envp)
+{
+	int		i;
+	char	**paths;
+	char	*single_path;
+	char	*single_path2;
+
+	i = 0;
+	while (ft_strncmp(envp[i], "PATH=", 5))
+		i++;
+	single_path = ft_substr(envp[i], 5, ft_strlen(envp[i]) - 5);
+	if (!single_path)
+		exit(EXIT_FAILURE);
+	paths = ft_split(single_path, ':');
+	free(single_path);
+	if (!paths)
+		exit(EXIT_FAILURE);
+	i = -1;
+	while (paths[++i])
+	{
+		single_path2 = ft_strjoin(paths[i], "/");
+		if (!single_path2)
+		{
+			i = -1;
+			while (paths[++i])
+				free(paths[i]);
+			exit(EXIT_FAILURE);
+		}
+		single_path = ft_strjoin(single_path2, args[0]);
+		free(single_path2);
+		if (!single_path)
+		{
+			i = -1;
+			while (paths[++i])
+				free(paths[i]);
+			exit(EXIT_FAILURE);
+		}
+		if (!access(single_path, X_OK))
+		{
+			i = -1;
+			while (paths[++i])
+				free(paths[i]);
+			free(single_path);
+			return (single_path);
+		}
+		free(single_path);
+	}
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	return (0);
+}
+
 static void	cmd_exec(char **args, char **envp)
 {
-	int	pid;
+	char	*path;
+	int		pid;
 
 	pid = fork();
 	if (!pid)
 	{
-		execve(args[0], args, envp);
-		printf("minishell: %s: command not found\n", args[0]);
-		exit(EXIT_FAILURE);
+		path = locate_path(args, envp);
+		if (!path)
+		{
+			printf("minishell: %s: command not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+		execve(path, args, envp);
 	}
 	waitpid(pid, 0, 0);
 }
