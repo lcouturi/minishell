3071ee0bf1a2176e329c2bb244456bedecffd9f3
Léopold Couturier-Otis <leo.couturier.otis@gmail.com>
2024-01-16 15:26:47 -0500

    handle space-only commands and don't add empty commands to history

--- main.c
+++ main.c
@@ -35,7 +35,6 @@ static void	cmd_exit(char **args)
 
 static void	cmd_exec(char **args, char **envp)
 {
-	int	i;
 	int	pid;
 
 	pid = fork();
@@ -43,40 +42,43 @@ static void	cmd_exec(char **args, char **envp)
 	{
 		execve(args[0], args, envp);
 		printf("minishell: %s: command not found\n", args[0]);
-		i = -1;
-		while (args[++i])
-			free(args[i]);
-		free(args);
 		exit(EXIT_FAILURE);
 	}
 	waitpid(pid, 0, 0);
+}
+
+static void	parser(char *str, char **envp)
+{
+	char	**args;
+	int		i;
+
+	args = ft_split(str, ' ');
+	if (!args)
+	{
+		free(str);
+		exit(EXIT_FAILURE);
+	}
+	if (args[0])
+	{
+		if (!ft_strncmp(args[0], "exit\0", 5))
+			cmd_exit(args);
+		else
+			cmd_exec(args, envp);
+	}
+	add_history(str);
+	free(str);
 	i = -1;
 	while (args[++i])
 		free(args[i]);
 	free(args);
 }
 
-static void	parser(char *str, char **envp)
-{
-	char	**args;
-
-	args = ft_split(str, ' ');
-	if (!args)
-		exit(EXIT_FAILURE);
-	if (!ft_strncmp(args[0], "exit\0", 5))
-		cmd_exit(args);
-	else
-		cmd_exec(args, envp);
-}
-
 int	main(int argc, char **argv, char **envp)
 {
-	int		ret;
 	char	*line;
 
 	(void)argc;
 	(void)argv;
-	ret = 1;
 	signal(SIGINT, handler);
 	while (1)
 	{
@@ -85,9 +87,6 @@ int	main(int argc, char **argv, char **envp)
 		{
 			if (ft_strncmp(line, "\0", 1))
 				parser(line, envp);
-			add_history(line);
-			free(line);
-			line = NULL;
 		}
 		else
 			printf("ctrl + d\n");
