6126ba33c4070e6c4f7e95818e7cfea6d3cb0949
Léopold Couturier-Otis <leo.couturier.otis@gmail.com>
2024-01-17 14:02:45 -0500

    add support for double quotes

--- parser/arg_splitter.c
+++ parser/arg_splitter.c
@@ -12,23 +12,37 @@
 
 #include "../include/minishell.h"
 
-static int	sep(char s, char c)
+static int	sep(char s, int *i)
 {
-	if (!s || s == c)
+	if (s == '\"')
+		return (1);
+	if (!i[3] && (!s || s == ' '))
 		return (1);
 	return (0);
 }
 
-static char	**loop(char const *s, char c, char **returned, int *i)
+static void	quote_check(char const *s, int *i)
+{
+	if (s[i[0]] == '\"')
+	{
+		if (!i[3])
+			i[3] = 1;
+		else
+			i[3] = 0;
+	}
+}
+
+static char	**loop(char const *s, char **returned, int *i)
 {
 	while (i[0]--)
 	{
-		if (i[0] && sep(s[i[0]], c) == 1 && sep(s[i[0] - 1], c) == 0)
+		quote_check(s, i);
+		if (i[0] && sep(s[i[0]], i) == 1 && sep(s[i[0] - 1], i) == 0)
 		{
 			i[1] = i[0]--;
-			while (i[0] && !(sep(s[i[0]], c) == 1 && sep(s[i[0] - 1], c) == 0))
+			while (i[0] && !(sep(s[i[0]], i) == 1 && sep(s[i[0] - 1], i) == 0))
 				i[0]--;
-			while (sep(s[i[0]], c) == 1)
+			while (sep(s[i[0]], i) == 1)
 				i[0]++;
 			i[2]--;
 			returned[i[2]] = ft_substr(s, i[0], i[1] - i[0]);
@@ -45,45 +59,41 @@ static char	**loop(char const *s, char c, char **returned, int *i)
 	return (returned);
 }
 
-static char	*whitespace_hack(char *s)
+static int	wordcount(char const *s)
 {
-	int	i;
-
-	i = -1;
-	while (++i)
-		if (s[i] == 9 || s[i] == 10 || s[i] == 11 || s[i] == 12 || s[i] == 13)
-			s[i] = ' ';
-	return (s);
-}
-
-static int	wordcount(char const *s, char c)
-{
-	int	i;
+	int	i[4];
 	int	wc;
 
-	i = 0;
+	i[0] = 0;
+	i[3] = 0;
 	wc = 0;
-	while (s && s[i])
+	while (s && s[i[0]])
 	{
-		if ((sep(s[i], c) == 0) && sep(s[i + 1], c) == 1)
+		quote_check(s, i);
+		if ((sep(s[i[0]], i) == 0) && sep(s[i[0] + 1], i) == 1)
 			wc++;
-		i++;
+		i[0]++;
 	}
 	return (wc);
 }
 
 char	**arg_splitter(char *s)
 {
-	int		i[3];
+	int		i[4];
+	int		j;
 	char	**returned;
 
-	s = whitespace_hack(s);
+	j = -1;
+	while (++j)
+		if (s[j] == 9 || s[j] == 10 || s[j] == 11 || s[j] == 12 || s[j] == 13)
+			s[j] = ' ';
 	i[1] = 0;
-	i[2] = wordcount(s, ' ');
+	i[2] = wordcount(s);
+	i[3] = 0;
 	returned = malloc((i[2] + 1) * 8);
 	if (!s || !returned)
 		return (0);
 	i[0] = ft_strlen(s) + 1;
 	returned[i[2]] = 0;
-	return (loop(s, ' ', returned, i));
+	return (loop(s, returned, i));
 }
