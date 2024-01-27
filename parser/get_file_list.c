3f5fad74c60af1ff608d889722734d2dd9942588
Léopold Couturier-Otis <leo.couturier.otis@gmail.com>
2024-01-27 15:10:03 -0500

    norminette fix

--- parser/get_file_list.c
+++ parser/get_file_list.c
@@ -33,13 +33,8 @@ static char	*lowest(t_list *lst)
 	return (ret);
 }
 
-static char	**load_lst(struct dirent *dr, DIR *dir, bool hidden)
+static void	load_lst_loop(t_list *lst, struct dirent *dr, DIR *dir, bool hidden)
 {
-	char	**files;
-	int		i;
-	t_list	*lst;
-
-	lst = ft_lstnew(ft_strdup(dr->d_name));
 	while (1)
 	{
 		dr = readdir(dir);
@@ -47,9 +42,20 @@ static char	**load_lst(struct dirent *dr, DIR *dir, bool hidden)
 			break ;
 		if (!hidden && dr->d_name[0] != '.')
 			ft_lstadd_back(&lst, ft_lstnew(ft_strdup(dr->d_name)));
-		if (hidden && dr->d_name[0] == '.' && ft_strncmp(dr->d_name, ".", 2) && ft_strncmp(dr->d_name, "..", 3))
+		if (hidden && dr->d_name[0] == '.' && ft_strncmp(dr->d_name, ".", 2)
+			&& ft_strncmp(dr->d_name, "..", 3))
 			ft_lstadd_back(&lst, ft_lstnew(ft_strdup(dr->d_name)));
 	}
+}
+
+static char	**load_lst(struct dirent *dr, DIR *dir, bool hidden)
+{
+	char	**files;
+	int		i;
+	t_list	*lst;
+
+	lst = ft_lstnew(ft_strdup(dr->d_name));
+	load_lst_loop(lst, dr, dir, hidden);
 	files = malloc(8 * ft_lstsize(lst) + 1);
 	i = -1;
 	while (++i < ft_lstsize(lst))
@@ -60,6 +66,7 @@ static char	**load_lst(struct dirent *dr, DIR *dir, bool hidden)
 		free(lst);
 		lst = lst->next;
 	}
+	closedir(dir);
 	return (files);
 }
 
@@ -67,11 +74,11 @@ char	**get_file_list(bool hidden)
 {
 	DIR				*dir;
 	struct dirent	*dr;
-	char			**files;
 
 	dir = opendir(".");
 	dr = readdir(dir);
-	while (hidden && dr && (dr->d_name[0] != '.' || !ft_strncmp(dr->d_name, ".", 2) || !ft_strncmp(dr->d_name, "..", 3)))
+	while (hidden && dr && (dr->d_name[0] != '.' || !ft_strncmp(dr->d_name, ".",
+				2) || !ft_strncmp(dr->d_name, "..", 3)))
 		dr = readdir(dir);
 	while (!hidden && dr && dr->d_name[0] == '.')
 		dr = readdir(dir);
@@ -80,9 +87,7 @@ char	**get_file_list(bool hidden)
 		closedir(dir);
 		return (0);
 	}
-	files = load_lst(dr, dir, hidden);
-	closedir(dir);
-	return (files);
+	return (load_lst(dr, dir, hidden));
 }
 
 void	match(char *str, char **split, char **files, int i)
