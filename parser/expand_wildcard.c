f624e92e209a51f32e108e65c35a6b8019bd1216
Léopold Couturier-Otis <leo.couturier.otis@gmail.com>
2024-01-29 13:28:55 -0500

    fit get_arg_num_loop into 25 lines

--- parser/expand_wildcard.c
+++ parser/expand_wildcard.c
@@ -18,31 +18,22 @@ static void	get_arg_num_loop(char **args, int *i, int *n)
 	int		n2;
 	char	**split;
 
-	i[0] = -1;
-	while (args[i[2]][++i[0]])
-	{
-		quote_check(args[i[2]], i);
-		if (!i[3] && !i[4] && args[i[2]][i[0]] == '*')
-		{
-			files = get_file_list(args[i[2]][0] == '.');
-			i[1] = -1;
-			split = ft_split(args[i[2]], '*');
-			while (files[++i[1]])
-				match(args[i[2]], split, files, i[1]);
-			strarrfree(split);
-			i[1] = -1;
-			n2 = 0;
-			while (files[++i[1]])
-				if (files[i[1]][0])
-					n2++;
-			if (!n2)
-				*n += 1;
-			else
-				*n += n2;
-			strarrfree(files);
-			break ;
-		}
-	}
+	files = get_file_list(args[i[2]][0] == '.');
+	i[1] = -1;
+	split = ft_split(args[i[2]], '*');
+	while (files[++i[1]])
+		match(args[i[2]], split, files, i[1]);
+	strarrfree(split);
+	i[1] = -1;
+	n2 = 0;
+	while (files[++i[1]])
+		if (files[i[1]][0])
+			n2++;
+	if (!n2)
+		*n += 1;
+	else
+		*n += n2;
+	strarrfree(files);
 }
 
 static int	get_arg_num(char **args)
@@ -53,7 +44,18 @@ static int	get_arg_num(char **args)
 	i[2] = -1;
 	n = 0;
 	while (args[++i[2]])
-		get_arg_num_loop(args, i, &n);
+	{
+		i[0] = -1;
+		while (args[i[2]][++i[0]])
+		{
+			quote_check(args[i[2]], i);
+			if (!i[3] && !i[4] && args[i[2]][i[0]] == '*')
+			{
+				get_arg_num_loop(args, i, &n);
+				break ;
+			}
+		}
+	}
 	return (n);
 }
 
