8d61dc97c273ea5ec3707c3040f26328f680432b
Léopold Couturier-Otis <leo.couturier.otis@gmail.com>
2024-01-29 13:38:52 -0500

    fix norminette issues with wildcard

--- parser/expand_wildcard.c
+++ parser/expand_wildcard.c
@@ -59,49 +59,54 @@ static int	get_arg_num(char **args)
 	return (n);
 }
 
-static void	wildcard_handler(char **args, char **newargs, int *i, int *i2)
+static void	wildcard_loop(char **files, int *i, char **newargs, char **args)
+{
+	bool	flag;
+
+	flag = 0;
+	while (files[++i[1]])
+	{
+		if (files[i[1]][0])
+		{
+			newargs[i[5]] = malloc(ft_strlen(files[i[1]]) + 3);
+			ft_strlcpy(newargs[i[5]], "\'", ft_strlen(files[i[1]]) + 3);
+			ft_strlcat(newargs[i[5]], files[i[1]], ft_strlen(files[i[1]]) + 3);
+			ft_strlcat(newargs[i[5]], "\'", ft_strlen(files[i[1]]) + 3);
+			i[5] += 1;
+			flag = 1;
+		}
+	}
+	if (!flag)
+	{
+		newargs[i[5]] = ft_strdup(args[i[2]]);
+		i[5] += 1;
+	}
+}
+
+static void	wildcard_handler(char **args, char **newargs, int *i)
 {
 	char	**files;
-	bool	flag;
 	char	**split;
 
 	files = get_file_list(args[i[2]][0] == '.');
-	flag = 0;
 	i[1] = -1;
 	split = ft_split(args[i[2]], '*');
 	while (files[++i[1]])
 		match(args[i[2]], split, files, i[1]);
 	strarrfree(split);
 	i[1] = -1;
-	while (files[++i[1]])
-	{
-		if (files[i[1]][0])
-		{
-			newargs[*i2] = malloc(ft_strlen(files[i[1]]) + 3);
-			ft_strlcpy(newargs[*i2], "\'", ft_strlen(files[i[1]]) + 3);
-			ft_strlcat(newargs[*i2], files[i[1]], ft_strlen(files[i[1]]) + 3);
-			ft_strlcat(newargs[*i2], "\'", ft_strlen(files[i[1]]) + 3);
-			*i2 += 1;
-			flag = 1;
-		}
-	}
-	if (!flag)
-	{
-		newargs[*i2] = ft_strdup(args[i[2]]);
-		*i2 += 1;
-	}
+	wildcard_loop(files, i, newargs, args);
 	strarrfree(files);
 }
 
 char	**expand_wildcard(char **args)
 {
-	int		i[5];
-	int		i2;
+	int		i[6];
 	char	**newargs;
 
 	newargs = malloc((strarrlen(args) + get_arg_num(args) + 1) * 8);
 	i[2] = -1;
-	i2 = 0;
+	i[5] = 0;
 	while (args[++i[2]])
 	{
 		i[0] = -1;
@@ -110,14 +115,14 @@ char	**expand_wildcard(char **args)
 			quote_check(args[i[2]], i);
 			if (!i[3] && !i[4] && args[i[2]][i[0]] == '*')
 			{
-				wildcard_handler(args, newargs, i, &i2);
+				wildcard_handler(args, newargs, i);
 				break ;
 			}
 		}
 		if (!args[i[2]][i[0]])
-			newargs[i2++] = ft_strdup(args[i[2]]);
+			newargs[i[5]++] = ft_strdup(args[i[2]]);
 	}
 	strarrfree(args);
-	newargs[i2] = 0;
+	newargs[i[5]] = 0;
 	return (newargs);
 }
