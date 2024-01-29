255a0aa3113d509e39c0d62cefce7fa8c4ce1243
Léopold Couturier-Otis <leo.couturier.otis@gmail.com>
2024-01-29 13:03:59 -0500

    don't touch args if wildcard matches nothing

--- parser/expand_wildcard.c
+++ parser/expand_wildcard.c
@@ -15,6 +15,7 @@
 static void	get_arg_num_loop(char **args, int *i, int *n)
 {
 	char	**files;
+	int		n2;
 	char	**split;
 
 	i[0] = -1;
@@ -30,9 +31,14 @@ static void	get_arg_num_loop(char **args, int *i, int *n)
 				match(args[i[2]], split, files, i[1]);
 			strarrfree(split);
 			i[1] = -1;
+			n2 = 0;
 			while (files[++i[1]])
 				if (files[i[1]][0])
-					*n += 1;
+					n2++;
+			if (!n2)
+				*n += 1;
+			else
+				*n += n2;
 			strarrfree(files);
 			break ;
 		}
@@ -54,9 +60,11 @@ static int	get_arg_num(char **args)
 static void	wildcard_handler(char **args, char **newargs, int *i, int *i2)
 {
 	char	**files;
+	bool	flag;
 	char	**split;
 
 	files = get_file_list(args[i[2]][0] == '.');
+	flag = 0;
 	i[1] = -1;
 	split = ft_split(args[i[2]], '*');
 	while (files[++i[1]])
@@ -72,7 +80,13 @@ static void	wildcard_handler(char **args, char **newargs, int *i, int *i2)
 			ft_strlcat(newargs[*i2], files[i[1]], ft_strlen(files[i[1]]) + 3);
 			ft_strlcat(newargs[*i2], "\'", ft_strlen(files[i[1]]) + 3);
 			*i2 += 1;
+			flag = 1;
 		}
+	}
+	if (!flag)
+	{
+		newargs[*i2] = ft_strdup(args[i[2]]);
+		*i2 += 1;
 	}
 	strarrfree(files);
 }
